b015fe5a5b76477304455ca5f7925021f859d9c9
Gabriel Muñumel <gabrielmunumel@gmail.com>
2015-03-25 05:28:02 +0000

    Added changes to the code
    Conflicts:
    	app/controllers/hq/orphans_controller.rb
    	app/views/hq/orphans/show.html.haml
    	spec/controllers/hq/orphans_controller_spec.rb
    	spec/views/hq/orphans/show_spec.rb

--- config/routes.rb
+++ config/routes.rb
@@ -12,7 +12,7 @@ Osra::Application.routes.draw do
     end
     resources :users, except: [:destroy]
     resources :sponsors, except: [:destroy]
-    resources :orphans, except: [:destroy]
+    resources :orphans, except: [:new, :create, :destroy]
   end
 
   devise_for :admin_users, ActiveAdmin::Devise.config
