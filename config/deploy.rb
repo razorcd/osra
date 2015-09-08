c2d720830cae94aff545f84fdd0d3d23a9ffff32
razor <razorcd@yahoo.com>
2015-09-08 16:17:00 +0300

    restart unicorn after deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -61,7 +61,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 # set :keep_releases, 5
 
 
-# after 'deploy:publishing', 'deploy:restart'
+after 'deploy:publishing', 'deploy:restart'
 
 namespace :deploy do
   # desc 'Restart Unicorn'
--- config/unicorn/production.rb
+++ config/unicorn/production.rb
@@ -1,10 +1,10 @@
 rails_env = "production"
 # app_path = "/home/rails/ig_#{rails_env}"
 app_path = "/home/deploy/osra/production"
-worker_processes 8
-listen "/tmp/unicorn.ig_#{rails_env}.sock"
-working_directory "current"
-pid "#{app_path}/shared/pids/unicorn.pid"
+worker_processes 1
+listen "/tmp/unicorn.osra_production.sock"
+working_directory "#{app_path}/current"
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
 stderr_path "#{app_path}/shared/log/unicorn.error.log"
 stdout_path "#{app_path}/shared/log/unicorn.log"
 
